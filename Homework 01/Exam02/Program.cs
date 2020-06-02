666ef9b6ce52b07f38a479b26e0bc2277a71d12e
Darko <fotinovskidarko@gmail.com>
2020-06-02 15:06:26 +0200

    added all exams

--- Homework 01/Exam02/Program.cs
+++ Homework 01/Exam02/Program.cs
@@ -11,22 +11,36 @@ namespace Exam02
             Console.WriteLine("Chek the date in this format(MM/DD/YYYY)");
             string inputDate = Console.ReadLine();
             DateTime date = DateTime.Parse(inputDate);
+            bool isLeapYear = DateTime.IsLeapYear(date.Year);
 
-            if ((date.Month == 1 && date.Day == 1) || (date.Month == 1 && date.Day == 7) ||
-                (date.Month == 3 && date.Day == 20) || (date.Month == 5 && date.Day == 1) ||
-                (date.Month == 8 && date.Day == 3) || (date.Month == 9 && date.Day == 8) ||
-                (date.Month == 10 && date.Day == 12) || (date.Month == 10 && date.Day == 23) ||
-                (date.Month == 12 && date.Day == 8))
+            while (true)
             {
-                Console.WriteLine($"This day is {date.DayOfWeek}, and its holiday");
-            }
-            else if (date.DayOfWeek >= DayOfWeek.Monday && date.DayOfWeek <= DayOfWeek.Friday)
-            {
-                Console.WriteLine($"This day is  {date.DayOfWeek} and is working day");
-            }
-            else
-            {
-                Console.WriteLine($"It's {date.DayOfWeek}, its weekend and its not working day");
+                if (isLeapYear && (date.Month == 2) && (date.Day >= 29))
+                {
+                    Console.WriteLine($"The date you entered {date} does not exist");
+                    break;
+                }
+
+
+                if ((date.Month == 1 && date.Day == 1) || (date.Month == 1 && date.Day == 7) ||
+                    (date.Month == 3 && date.Day == 20) || (date.Month == 5 && date.Day == 1) ||
+                    (date.Month == 8 && date.Day == 3) || (date.Month == 9 && date.Day == 8) ||
+                    (date.Month == 10 && date.Day == 12) || (date.Month == 10 && date.Day == 23) ||
+                    (date.Month == 12 && date.Day == 8))
+                {
+                    Console.WriteLine($"This day is {date.DayOfWeek}, and its holiday");
+                    break;
+                }
+                else if (date.DayOfWeek >= DayOfWeek.Monday && date.DayOfWeek <= DayOfWeek.Friday)
+                {
+                    Console.WriteLine($"This day is  {date.DayOfWeek} and is working day");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"It's {date.DayOfWeek}, its weekend and its not working day");
+                    break;
+                }
             }
 
 
