8d6f6f454d40bcd66640562188f1c1e96308ab35
Darko <fotinovskidarko@gmail.com>
2020-06-02 13:45:21 +0200

    add homework

---
+++ Homework 01/SEDC.Project.C-sharp.Advanced/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SEDC.Project.C_sharp.Advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region First Task
+
+            List<string> names = new List<string>();
+
+
+
+            while (true)
+            {
+                Console.WriteLine("Enter your name or press 'x' to exit.");
+                string input = Console.ReadLine();
+                if (input != "x")
+                {
+                    names.Add(input);
+
+                }
+                else break;
+
+            }
+
+            Console.WriteLine("Enter text here:");
+            string inputText = Console.ReadLine();
+
+            List<string> trimmedInputList = inputText.Split(" ").ToList();
+
+            foreach (string name in names)
+            {
+                
+                var count = trimmedInputList.Where()
+
+
+            }
+
+            #endregion
+
+            #region Second Task
+
+
+
+            #endregion
+            /*Console.ReadLine();*/
+        }
+    }
+}
